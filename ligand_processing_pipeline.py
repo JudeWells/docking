58460c9f3594a41fea4f16266a89998e51aefda2
Jude Wells <judewells@hotmail.com>
2022-08-17 01:17:06 +0100

    add prints for debugging

--- ligand_processing_pipeline.py
+++ ligand_processing_pipeline.py
@@ -80,15 +80,17 @@ def lig_mol2_2_pdbqt(ligand_in_path):
     ligand_dir, ligname = os.path.split(ligand_in_path)
     ligand_out_path = os.path.join(ligand_dir, ligname.split('.')[0])
     cmd_ = gen_cmd_str(cmd_dict['mol22pdbqt'], '%s' %(ligand_in_path), '%s.pdbqt'%(ligand_out_path))
+    print(cmd_)
     exec_shell_command(cmd_)
     os.remove(ligand_in_path)
 
 def prepare_ligand_runner(lig_dir):
     out_dir = os.path.join(lig_dir, 'processed')
     os.makedirs(out_dir, exist_ok=True)
-    # lig_fnames = [f for f in os.listdir(lig_dir) if '.sdf' in f]
-    # for lig in lig_fnames:
-    #     lig_sdf_2_mol2(os.path.join(lig_dir, lig))
+    lig_fnames = [f for f in os.listdir(lig_dir) if '.sdf' in f]
+    for lig in lig_fnames:
+        lig_sdf_2_mol2(os.path.join(lig_dir, lig))
+        break
     mol2_ligs = [f for f in os.listdir(out_dir) if '.mol2' in f]
     for lig in mol2_ligs:
         lig_mol2_2_pdbqt(os.path.join(out_dir, lig))
