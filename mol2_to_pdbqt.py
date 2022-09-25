77d2fa01f2c4cb7fe2293d604517c03f67f227af
Jude Wells <judewells@hotmail.com>
2022-09-25 19:21:05 +0100

    fix bug

--- mol2_to_pdbqt.py
+++ mol2_to_pdbqt.py
@@ -2,6 +2,6 @@ import os
 from ligand_processing_pipeline import lig_mol2_2_pdbqt
 
 lig_dir = 'enamine_collections/ess_frag320/processed'
-for f in lig_dir:
+for f in os.listdir(lig_dir):
     in_path = os.path.join(lig_dir, f)
     lig_mol2_2_pdbqt(in_path, del_mol2=False)
