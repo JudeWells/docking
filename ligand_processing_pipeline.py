3b6c50d5e417f4d4e7a7e63cefdc751b0357c111
Jude Wells <judewells@hotmail.com>
2022-09-25 17:45:31 +0100

    fix bash command calls

--- ligand_processing_pipeline.py
+++ ligand_processing_pipeline.py
@@ -17,17 +17,17 @@ cmd_dict = {
    'pdb2pqr': 'pdb2pqr --ff=amber --chain --with-ph=7.4 --verbose %s %s',
    'pqr2pdbqt': 'prepare_receptor4.py -r %s -o %s -C -U nphs_lps -v',
   ### ligand prep
-   'split_bulk': '/home/jwells/Tools/bin/obabel %s -O %s -m',
-   'split_bulk_sdf2mol2': '/home/jwells/Tools/bin/obabel -isdf %s -omol2 -O %s -m',
+   'split_bulk': 'obabel %s -O %s -m',
+   'split_bulk_sdf2mol2': 'obabel -isdf %s -omol2 -O %s -m',
    # choose either:
-   'smi2mol2': '/home/jwells/Tools/bin/obabel %s -O %s --gen3d --best --canonical --conformers --weighted --nconf 50 --ff GAFF',
+   'smi2mol2': 'obabel %s -O %s --gen3d --best --canonical --conformers --weighted --nconf 50 --ff GAFF',
    # or:
-   'smi2mol2_ph74': '/home/jwells/Tools/bin/obabel %s -O %s --gen3d best -p 7.4 --canonical',
+   'smi2mol2_ph74': 'obabel %s -O %s --gen3d best -p 7.4 --canonical',
    # or:
-   'smi2mol2_gaff': '/home/jwells/Tools/bin/obabel %s -O %s --gen3d --best --canonical --minimize --ff GAFF --steps 10000 --sd',
-   'mol22pdbqt': '/SAN/orengolab/nsp13/docking/miniconda3/bin/prepare_ligand4.py -l %s -o %s -U nphs_lps -v',
+   'smi2mol2_gaff': 'obabel %s -O %s --gen3d --best --canonical --minimize --ff GAFF --steps 10000 --sd',
+   'mol22pdbqt': 'prepare_ligand4.py -l %s -o %s -U nphs_lps -v',
   ### docking
-   'split_bulk_pdbqt2pdb': '/home/jwells/Tools/bin/obabel -ipdbqt %s -opdb -O %s -m',
+   'split_bulk_pdbqt2pdb': 'obabel -ipdbqt %s -opdb -O %s -m',
 }
 
 def gen_cmd_str(cmd_str, infile, outfile):
@@ -74,14 +74,16 @@ def lig_sdf_2_mol2(ligand_in_path):
     if not os.path.exists(os.path.split(out_dir)[0]):
         os.makedirs(out_dir)
     ligand_out_path = os.path.join(out_dir, ligname).split('.mol2')[0]
-    cmd_ = gen_cmd_str(cmd_dict['smi2mol2'], '%s' % (ligand_in_path), '%s.mol2' % (ligand_out_path))
+    # cmd_ = gen_cmd_str(cmd_dict['smi2mol2'], '%s' % (ligand_in_path), '%s.mol2' % (ligand_out_path))
+    cmd_ = f"obabel {ligand_in_path} -O {ligand_out_path}.mol2 --gen3d --best --canonical --conformers --weighted --nconf 50 --ff GAFF"
     print(cmd_)
     exec_shell_command(cmd_)
 
 def lig_mol2_2_pdbqt(ligand_in_path, del_mol2=True):
     ligand_dir, ligname = os.path.split(ligand_in_path)
     ligand_out_path = os.path.join(ligand_dir, ligname.split('.')[0] + '.pdbqt')
-    cmd_ = gen_cmd_str(cmd_dict['mol22pdbqt'], '%s' %(ligand_in_path), '%s'%(ligand_out_path))
+    # cmd_ = gen_cmd_str(cmd_dict['mol22pdbqt'], '%s' %(ligand_in_path), '%s'%(ligand_out_path))
+    cmd_ = f'prepare_ligand4.py -l {ligand_in_path} -o {ligand_out_path} -U nphs_lps -v'
     print(cmd_)
     exec_shell_command(cmd_)
     if os.path.exists(ligand_out_path + '.pdbqt') and del_mol2:
@@ -101,7 +103,6 @@ def prepare_ligand_runner(lig_dir):
     lig_fnames = [f for f in os.listdir(lig_dir) if '.sdf' in f]
     for lig in lig_fnames:
         lig_sdf_2_mol2(os.path.join(lig_dir, lig))
-        break
     mol2_ligs = [f for f in os.listdir(out_dir) if '.mol2' in f]
     for lig in mol2_ligs:
         lig_mol2_2_pdbqt(os.path.join(out_dir, lig))
