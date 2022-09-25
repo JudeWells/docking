60889c2bcd50175f7db768c7fc4341eba0695770
Jude Wells <judewells@hotmail.com>
2022-09-25 19:55:00 +0100

    fix bug

--- mol2pdbqt.sh
+++ mol2pdbqt.sh
@@ -3,5 +3,5 @@ if ! test -f "file_list.txt";
   then ls > file_list.txt
 fi
 fname="$(sed "${1}q;d" file_list.txt)"
-prepare_ligand4.py -l $fname -o $fname$.pdbqt -U nphs_lps -v
+prepare_ligand4.py -l $fname -o ${fname}.pdbqt -U nphs_lps -v
 
